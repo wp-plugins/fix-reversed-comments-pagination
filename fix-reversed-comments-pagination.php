78bffb8a77c1fbe7ca3b946e6752f6ba5792fe48
msafi <msafi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-12 17:09:32 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fix-reversed-comments-pagination/trunk@372038 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ fix-reversed-comments-pagination.php
@@ -0,0 +1,173 @@
+<?php
+/*
+Plugin Name: Fix Reversed Comments Pagination
+Plugin URI: http://winkpress.com/articles/fix-reversed-comments-pagination/
+Description: Fixes a problem with reversed comments pagination. See <a href="http://winkpress.com/articles/fix-reversed-comments-pagination/">this</a> for more details.
+Author: WinkPress
+Version: 0.5
+Author URI: http://winkpress.com/
+*/
+function wink_good_discussion_settings()
+{
+  // If comments are not paginated, newer to oldest, and last page first
+  if (!get_option('page_comments'))
+    return false;
+
+  // If comments are not newer to oldest
+  if (get_option('comment_order') != 'desc')
+    return false;
+
+  // If last page not displayed first
+  if (get_option('default_comments_page') != 'newest')
+    return false;
+
+  // Otherwise,
+    return true;
+}
+
+if (wink_good_discussion_settings()):
+
+add_action('comment_form', 'wink_comment_redirect');
+add_filter('comment_post_redirect', 'wink_comment_redirect', 99, 2);
+
+function wink_comment_redirect($location = '', $comment = null)
+{
+  if (!$comment)
+  {
+    $cpage = get_query_var('cpage');
+    $link = get_comments_pagenum_link( $cpage );
+    $link = str_replace('#comments', '', $link);
+    echo "<input type='hidden' value='$link' name='redirect_to' />";
+  }
+  else
+  {
+    if ($comment->comment_parent == 0)
+    {
+      $location = get_permalink($comment->comment_post_ID);
+      $location .= '#comment-' . $comment->comment_ID;
+    }
+
+    return $location;
+  }
+}
+
+class Walker_Comment_Wink extends Walker_Comment
+{
+  /*
+   This is copied from class-wp-walker.php and modified slightly.
+   Thanks to jpowermacg5 from irc://irc.freenode.net/php for helping
+   me figure this out!
+  */
+  function paged_walk( $elements, $max_depth, $page_num, $per_page ) {
+    /* sanity check */
+    if ( empty($elements) || $max_depth < -1 )
+      return '';
+
+    $args = array_slice( func_get_args(), 4 );
+    $output = '';
+
+    $id_field = $this->db_fields['id'];
+    $parent_field = $this->db_fields['parent'];
+
+    $count = -1;
+    if ( -1 == $max_depth )
+      $total_top = count( $elements );
+    if ( $page_num < 1 || $per_page < 0  ) {
+      // No paging
+      $paging = false;
+      $start = 0;
+      if ( -1 == $max_depth )
+        $end = $total_top;
+      $this->max_pages = 1;
+    } else {
+      $paging = true;
+      $start = ( (int)$page_num - 1 ) * (int)$per_page;
+      $end   = $start + $per_page;
+      if ( -1 == $max_depth )
+        $this->max_pages = ceil($total_top / $per_page);
+    }
+
+    // flat display
+    if ( -1 == $max_depth ) {
+      if ( !empty($args[0]['reverse_top_level']) ) {
+        $elements = array_reverse( $elements );
+
+        $page_num = 1 + ceil($total_top / $per_page) - $page_num;
+
+        $start = ($page_num - 1) * $per_page;
+        $end = $start + $per_page;
+      }
+
+      $empty_array = array();
+      foreach ( $elements as $e ) {
+        $count++;
+        if ( $count < $start )
+          continue;
+        if ( $count >= $end )
+          break;
+        $this->display_element( $e, $empty_array, 1, 0, $args, $output );
+      }
+      return $output;
+    }
+
+    /*
+     * separate elements into two buckets: top level and children elements
+     * children_elements is two dimensional array, eg.
+     * children_elements[10][] contains all sub-elements whose parent is 10.
+     */
+    $top_level_elements = array();
+    $children_elements  = array();
+    foreach ( $elements as $e) {
+      if ( 0 == $e->$parent_field )
+        $top_level_elements[] = $e;
+      else
+        $children_elements[ $e->$parent_field ][] = $e;
+    }
+
+    $total_top = count( $top_level_elements );
+    if ( $paging )
+      $this->max_pages = ceil($total_top / $per_page);
+    else
+      $end = $total_top;
+
+    if ( !empty($args[0]['reverse_top_level']) ) {
+      $top_level_elements = array_reverse( $top_level_elements );
+
+      $page_num = 1 + ceil($total_top / $per_page) - $page_num;
+
+      $start = ($page_num - 1) * $per_page;
+      $end = $start + $per_page;
+    }
+
+    if ( !empty($args[0]['reverse_children']) ) {
+      foreach ( $children_elements as $parent => $children )
+        $children_elements[$parent] = array_reverse( $children );
+    }
+
+    foreach ( $top_level_elements as $e ) {
+      $count++;
+
+      //for the last page, need to unset earlier children in order to keep track of orphans
+      if ( $end >= $total_top && $count < $start )
+          $this->unset_children( $e, $children_elements );
+
+      if ( $count < $start )
+        continue;
+
+      if ( $count >= $end )
+        break;
+
+      $this->display_element( $e, $children_elements, $max_depth, 0, $args, $output );
+    }
+
+    if ( $end >= $total_top && count( $children_elements ) > 0 ) {
+      $empty_array = array();
+      foreach ( $children_elements as $orphans )
+        foreach( $orphans as $op )
+          $this->display_element( $op, $empty_array, 1, 0, $args, $output );
+    }
+
+    return $output;
+  }
+}
+endif;
---
+++ readme.txt
@@ -0,0 +1,57 @@
+=== Fix Reversed Comments Pagination ===
+Contributors: msafi
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7663346
+Tags: comments, pagination
+Requires at least: 3.1
+Tested up to: 3.1.1
+Stable tag: trunk
+
+Fixes WordPress reversed comments pagination issue where it doesn't fill the default page with comments.
+
+== Description ==
+### What Problem Does This Plugin Fix? ###
+
+![Discussion settings](http://winkpress.com/wp-content/uploads/2011/04/discussion-settings.png)
+
+If your WordPress discussion settings are like the [screenshot](http://winkpress.com/wp-content/uploads/2011/04/discussion-settings.png) and you have 21 comments on a certain post, here’s how WordPress will display the comments:
+
+Default page will show only one comment, #21
+
+The second page will show comments 20 to 1.
+
+What WordPress should do instead is display comments 21 to 2 on the default page, and 1 on the second page. And that's what this plugin does.
+
+### Installation ###
+
+Upload and activate. **Then** you must make a minor modification to your theme.
+
+Look for `wp_list_comments` in your theme. It's probably in `comments.php`.
+
+Before you call `wp_list_comments`, you must create a new PHP class to display the comments. Then add it to the `wp_list_comments` call -- like this:
+
+`if (class_exists('Walker_Comment_Wink'))
+  $walker = new Walker_Comment_Wink();
+else
+  $walker = '';
+ 
+wp_list_comments(array('walker' => $walker));`
+
+Note that the plugin will only take effect if your comments display settings are exactly like the red circles in the screenshot above. If your settings are different, the plugin won’t interfere with how your comments are displayed.
+
+### One Thing to Consider ###
+
+With this implementation of pagination, some times comments move from one page to another. So, in some cases, a permalink that leads to one particular comment might no longer work. For example, if you have a comment that appears on page one, and another blogger links to that comment directly:
+
+`http://example.com/controversial-post/comment-page-1/#comment-134`
+
+The comment could after awhile move to page 2 of comments and the link will no longer lead to the right comment.
+
+### Bug Reports & Issues ###
+
+I’m using this plugin on my site and have tested it else where. It works great. But your setup might be different and you may notice things I missed. If that’s the case, tell me in the [comments of the plugin's page](http://winkpress.com/articles/fix-reversed-comments-pagination/).
+
+Developed by [WinkPress](http://winkpress.com/).
+== Changelog ==
+
+= 0.5 =
+* Initial release
